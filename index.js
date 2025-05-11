e6ae60c26f71550c6936eee5b30117f4ce815375
heywon0909 <hw731911@gmail.com>
2025-05-11 20:32:53 +0900

    feat: import rule 설정

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import eslint from './rules/eslint';
-import jsxA11y from './rules/jsxA11y';
-import typescript from './rules/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslint from './rules/eslint'
+import jsxA11y from './rules/jsxA11y'
+import typescript from './rules/typescript'
+import importPlugin from './rules/import'
 
 export default [
-    eslintConfigPrettier,
-    {
-      	languageOptions: {
-			globals: {
-				everything: true,
-			},
-		},
-        rules:{
-            ...eslint,...jsxA11y,...typescript
-        }
-    }
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        everything: true,
+      },
+    },
+    rules: {
+      ...eslint,
+      ...jsxA11y,
+      ...typescript,
+      ...importPlugin,
+    },
+  },
 ]
