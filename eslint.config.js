ed575789abcda4eab1909409c09e6cc4934bf59b
heywon0909 <hw731911@gmail.com>
2025-06-15 12:04:29 +0900

    style: eslint rule 에 따라 import/order 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
 import globals from 'globals';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import { FlatCompat } from '@eslint/eslintrc';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
-import eslint from '@eslint/js';
-import react from 'eslint-plugin-react';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
